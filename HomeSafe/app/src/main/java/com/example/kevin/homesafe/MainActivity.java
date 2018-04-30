e8e967b79d25a2b94dee1d76192f58214d6f337d
Kevin Feng <kfeng64@gmail.com>
2018-04-29 20:08:05 -0400

    Time Display

--- HomeSafe/app/src/main/java/com/example/kevin/homesafe/MainActivity.java
+++ HomeSafe/app/src/main/java/com/example/kevin/homesafe/MainActivity.java
@@ -5,16 +5,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 
 public class MainActivity extends AppCompatActivity {
     private TimePicker timePicker1;
     private Switch willContact;
+    private TextView time;
     private int hour, min;
-
-
-
+    private String format = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         timePicker1 = findViewById(R.id.timePicker1);
+        time = findViewById(R.id.time);
         willContact = findViewById(R.id.switch1);
 
         willContact.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -30,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
                 if (isChecked) {
                     hour = timePicker1.getHour();
                     min = timePicker1.getMinute();
+                    showTime(hour, min);
+                } else {
+                    time.setText("");
                 }
             }
         });
@@ -43,5 +47,28 @@ public class MainActivity extends AppCompatActivity {
         int hour = timePicker1.getHour();
         int min = timePicker1.getMinute();
         Log.i("time:", "hour" + hour + " minute" + min);
+    }
+
+    public void showTime(int hour, int min) {
+        String minFormat;
+        if (hour == 0) {
+            hour += 12;
+            format = "AM";
+        } else if (hour == 12) {
+            format = "PM";
+        } else if (hour > 12) {
+            hour -= 12;
+            format = "PM";
+        } else {
+            format = "AM";
+        }
+        if (min < 10) {
+            minFormat = "0" + Integer.toString(min);
+        } else {
+            minFormat = Integer.toString(min);
+        }
+
+        time.setText(new StringBuilder().append(hour).append(" : ").append(minFormat)
+                .append(" ").append(format));
     }
 }
