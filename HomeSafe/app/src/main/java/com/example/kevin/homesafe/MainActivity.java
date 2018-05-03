b886d268b007d7448c87b006645a1de05ff34b42
Kevin Feng <kfeng64@gmail.com>
2018-05-03 18:48:41 -0400

    add friend buttons and beginning layout

--- HomeSafe/app/src/main/java/com/example/kevin/homesafe/MainActivity.java
+++ HomeSafe/app/src/main/java/com/example/kevin/homesafe/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.kevin.homesafe;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -24,27 +30,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        final TextView phoneNumber = findViewById(R.id.phoneNumber);
+        final TextView name = findViewById(R.id.name);
 
         final Button button = findViewById(R.id.loginButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                user = new User(phoneNumber.getText().toString());
+                user = new User(name.getText().toString());
                 setContentView(R.layout.activity_main);
-                setTime();
+                settingTimePage();
             }
         });
 
-
-
-
-
     }
 
-    public void setTime() {
+    public void settingTimePage() {
         timePicker1 = findViewById(R.id.timePicker1);
         time = findViewById(R.id.time);
         willContact = findViewById(R.id.switch1);
+        final FloatingActionButton addFriend1 = findViewById(R.id.addFriend1);
+        final FloatingActionButton addFriend2 = findViewById(R.id.addFriend2);
+        final FloatingActionButton addFriend3 = findViewById(R.id.addFriend3);
+        final FloatingActionButton addFriend4 = findViewById(R.id.addFriend4);
+
+//        TextView debug =  findViewById(R.id.debug);
+//        debug.setText(user.name);
 
         willContact.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -59,10 +68,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        Log.i("time:", "hour" + hour + " minute" + min);
+        addFriend1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFriend(addFriend1);
+            }
+        });
+        addFriend2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFriend(addFriend2);
+            }
+        });
+        addFriend3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFriend(addFriend3);
+            }
+        });
+        addFriend4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFriend(addFriend4);
+            }
+        });
+
+
+
     }
 
-    public void showTime(int hour, int min) {
+    public void addFriend(FloatingActionButton friend) {
+        //just do new window
+    }
+
+    private void showTime(int hour, int min) {
         String minFormat;
         if (hour == 0) {
             hour += 12;
--- HomeSafe/app/src/main/java/com/example/kevin/homesafe/User.java
+++ HomeSafe/app/src/main/java/com/example/kevin/homesafe/User.java
@@ -3,21 +3,22 @@ package com.example.kevin.homesafe;
 
 
 public class User {
-    protected String phoneNumber;
+    protected String name;
     protected Friend[] friends;
 
-    public User (String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public User (String name) {
+        this.name = name;
         this.friends = new Friend[4];
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public String getName() {
+        return name;
     }
 
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public void setName(String name) {
+        this.name = name;
     }
+
 
 }
